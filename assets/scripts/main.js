c5177fece45d2fdeb8ca048cf12536627a7553ab
Samuel Webster <sam@manyhatsdigital.com>
2017-05-11 18:11:12 +0200

    further work on header, added some js for a mobile navigation menu slider and uploaded double trouble logo.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -10,68 +10,94 @@
  * always reference jQuery with $, even when in .noConflict() mode.
  * ======================================================================== */
 
-(function($) {
+(function ($) {
 
-  // Use this variable to set up the common and page specific functions. If you
-  // rename this variable, you will also need to rename the namespace below.
-  var Sage = {
-    // All pages
-    'common': {
-      init: function() {
-        // JavaScript to be fired on all pages
-      },
-      finalize: function() {
-        // JavaScript to be fired on all pages, after page specific JS is fired
-      }
-    },
-    // Home page
-    'home': {
-      init: function() {
-        // JavaScript to be fired on the home page
-      },
-      finalize: function() {
-        // JavaScript to be fired on the home page, after the init JS
-      }
-    },
-    // About us page, note the change from about-us to about_us.
-    'about_us': {
-      init: function() {
-        // JavaScript to be fired on the about us page
-      }
-    }
-  };
+    // Use this variable to set up the common and page specific functions. If you
+    // rename this variable, you will also need to rename the namespace below.
+    var Sage = {
+        // All pages
+        'common': {
+            init: function () {
+                // JavaScript to be fired on all pages
+            },
+            finalize: function () {
+                // JavaScript to be fired on all pages, after page specific JS is fired
+                (function ($) {
+                    // Initialize Slidebars
+                    var Controller = new Slidebars();
+                    Controller.init();
+                    // Toggle Slidebars
+                    $( '.toggle-mobile-nav' ).on( 'click', function ( event ) {
+                        // Stop default action and bubbling
+                        event.stopPropagation();
+                        event.preventDefault();
+                        // Toggle the Slidebar with id 'mobile-nav'
+                        Controller.toggle( 'mobile-nav' );
+                        // Click off menu close
+                        $( Controller.events ).on( 'opened', function () {
+                            $( '[canvas="container"]' ).addClass( 'js-close-any-slidebar' );
+                        } );
 
-  // The routing fires all common scripts, followed by the page specific scripts.
-  // Add additional events for more control over timing e.g. a finalize event
-  var UTIL = {
-    fire: function(func, funcname, args) {
-      var fire;
-      var namespace = Sage;
-      funcname = (funcname === undefined) ? 'init' : funcname;
-      fire = func !== '';
-      fire = fire && namespace[func];
-      fire = fire && typeof namespace[func][funcname] === 'function';
+                        $( Controller.events ).on( 'closed', function () {
+                            $( '[canvas="container"]' ).removeClass( 'js-close-any-slidebar' );
+                        } );
 
-      if (fire) {
-        namespace[func][funcname](args);
-      }
-    },
-    loadEvents: function() {
-      // Fire common init JS
-      UTIL.fire('common');
+                        $( 'body' ).on( 'click', '.js-close-any-slidebar', function ( event ) {
+                            event.stopPropagation();
+                            Controller.close();
+                        } );
+                    } );
+                })(jQuery);
+            }
+        },
+        // Home page
+        'home': {
+            init: function () {
+                // JavaScript to be fired on the home page
+            },
+            finalize: function () {
+                // JavaScript to be fired on the home page, after the init JS
+            }
+        },
+        // About us page, note the change from about-us to about_us.
+        'about_us': {
+            init: function () {
+                // JavaScript to be fired on the about us page
+            }
+        }
+    };
 
-      // Fire page-specific init JS, and then finalize JS
-      $.each(document.body.className.replace(/-/g, '_').split(/\s+/), function(i, classnm) {
-        UTIL.fire(classnm);
-        UTIL.fire(classnm, 'finalize');
-      });
+    // The routing fires all common scripts, followed by the page specific scripts.
+    // Add additional events for more control over timing e.g. a finalize event
+    var UTIL = {
+        fire: function (func, funcname, args) {
+            var fire;
+            var namespace = Sage;
+            funcname = (funcname === undefined) ? 'init' : funcname;
+            fire = func !== '';
+            fire = fire && namespace[func];
+            fire = fire && typeof namespace[func][funcname] === 'function';
 
-      // Fire common finalize JS
-      UTIL.fire('common', 'finalize');
-    }
-  };
+            if (fire) {
+                namespace[func][funcname](args);
+            }
+        },
+        loadEvents: function () {
+            // Fire common init JS
+            UTIL.fire('common');
 
-  // Load Events
-  $(document).ready(UTIL.loadEvents);
+            // Fire page-specific init JS, and then finalize JS
+            $.each(document.body.className.replace(/-/g, '_').split(/\s+/), function (i, classnm) {
+                UTIL.fire(classnm);
+                UTIL.fire(classnm, 'finalize');
+            });
+
+            // Fire common finalize JS
+            UTIL.fire('common', 'finalize');
+        }
+    };
+
+    // Load Events
+    $(document).ready(UTIL.loadEvents);
 
 })(jQuery); // Fully reference jQuery after this point.
--- templates/header.php
+++ templates/header.php
@@ -1,5 +1,8 @@
 <header class="banner">
   <div class="full-width-container">
+      <div class="toggle-mobile-nav hidden-md-up">
+          <div class="icon-mobile_menu"></div>
+      </div>
       <div class="shipping-banner">Free Shipping Worldwide</div>
   </div>
   <div class="container">
@@ -7,19 +10,18 @@
     <nav class="nav-top">
 
 <!-- left side of the navigation menu -->
-
         <?php
       if (has_nav_menu('top_left_navigation')) :
-        wp_nav_menu(['theme_location' => 'top_left_navigation', 'menu_class' => 'nav', 'fallback_cb' => false]);
+        wp_nav_menu(['theme_location' => 'top_left_navigation', 'menu_class' => 'nav hidden-sm-down', 'fallback_cb' => false]);
       endif;
       ?>
 <!-- space for centered logo -->
-        
-      
+
+
 <!-- right side of the navigation menu -->
       <?php
       if (has_nav_menu('top_right_navigation')) :
-	      wp_nav_menu(['theme_location' => 'top_right_navigation', 'menu_class' => 'nav', 'fallback_cb' => false]);
+	      wp_nav_menu(['theme_location' => 'top_right_navigation', 'menu_class' => 'nav hidden-sm-down', 'fallback_cb' => false]);
       endif;
       ?>
 
